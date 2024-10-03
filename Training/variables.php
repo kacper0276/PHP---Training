822f424994076914ae7733a95bed7b7481217004
kacper0276 <kacper0276@op.pl>
2024-10-03 21:36:47 +0200

    Variables

---
+++ Training/variables.php
@@ -0,0 +1,17 @@
+<?php
+
+// First echo
+echo "<h1>Hello, World</h1> \n"; 
+
+$age = 11;
+
+echo "$age\n";
+
+$arr1 = [];
+$arr2 = array();
+
+// Check variable type
+var_dump($age);
+
+const constVariable = "Const";
+define('CONSTVARIABLE', 10);
