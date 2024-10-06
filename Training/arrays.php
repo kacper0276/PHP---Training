9a89fa32f79b265d2d3d7172e15d6c93a8db9839
kacper0276 <kacper0276@op.pl>
2024-10-06 22:05:12 +0200

    Tablice - podstawa

---
+++ Training/arrays.php
@@ -0,0 +1,10 @@
+<?php
+
+$employee1 = 'Jan Kowalski';
+$employee2 = 'Zbigniew Nowak';
+$employee3 = 'Jadwiga Kaczmarska';
+
+// zapis tablicowy 5.3
+$user = ['Jan Kowalski', 'Zbigniew Nowak', 'Jadwiga Kaczmarska'];
+$user = [$employee1, $employee2, $employee3];
+var_dump($user);
