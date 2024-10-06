101d6660c46aa4de3f190e272d6f30ff0417b71f
kacper0276 <kacper0276@op.pl>
2024-10-07 00:05:09 +0200

    Dodawanie nowej wartości

--- Training/arrays.php
+++ Training/arrays.php
@@ -8,3 +8,24 @@ $employee3 = 'Jadwiga Kaczmarska';
 $user = ['Jan Kowalski', 'Zbigniew Nowak', 'Jadwiga Kaczmarska'];
 $user = [$employee1, $employee2, $employee3];
 var_dump($user);
+
+/*   
+Zmiana wartości
+*/
+
+// $index = 11;
+
+// $users = ['Jan Kowalski', 'Zbigniew Nowak', 'Jadwiga Kaczmarska'];
+// print_r($users);
+// $users[$index] = 'Anna Miłosz';
+// print_r($users);
+
+/* 
+   W ten oto sposób zachowaliśmy starą wartość i ustawiliśmy nową.
+   Teraz dodamy nową wartość do tablicy.
+   Aby to zrobić nie musimy znać wartości najwyższego indeksu pod którym mamy dane.
+   PHP zrobi to za nas.
+ */
+
+// $users = ['Jan Kowalski', 'Zbigniew Nowak', 'Jadwiga Kaczmarska'];
+// $users[] = 'Adrian Onyszko';
