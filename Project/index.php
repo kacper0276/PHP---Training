d256c42c4321bcbb9a6258ac6ad2e81166170c5e
kacper0276 <kacper0276@op.pl>
2024-10-09 21:58:41 +0200

    Basic html template

--- Project/index.php
+++ Project/index.php
@@ -1,5 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
+namespace App;
+
 $test = 'test';
 
 // include ('src/Utils/debug.php'); // On też pokazuje błąd ale nie przerywa działania skryptu (Warning)
@@ -8,4 +12,36 @@ $test = 'test';
 // require ('src/Utils/debug.php'); // Przerywa działanie skryptu w miejscu kiedy nie może wczytać pliku (Fatal error)
 require_once ('src/Utils/debug.php');
 
-dumb($test);
+?>
+
+<html>
+    <head>
+
+    </head>
+    <body>
+        <div class="header">
+            <h1>Moje notatki</h1>
+        </div>
+
+        <div class="container">
+            <div class="menu">
+                <ul>
+                    <li>
+                        <a href="/">Lista notatek</a>
+                    </li>
+                    <li>
+                        <a href="/?action=create">Nowa notatka</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                Treść
+            </div>
+        </div>
+
+        <div class="footer">
+
+        </div>
+    </body>
+</html>
