4961f14f29bf2bcc0a1adf86dc5838e206638c1b
kacper0276 <kacper0276@op.pl>
2024-10-10 22:42:56 +0200

    Changes in structure in create.php and list.php

--- Project/templates/pages/create.php
+++ Project/templates/pages/create.php
@@ -1,32 +1,6 @@
-<html>
-    <head>
-
-    </head>
-    <body>
-        <div class="header">
-            <h1>Moje notatki</h1>
-        </div>
-
-        <div class="container">
-            <div class="menu">
-                <ul>
-                    <li>
-                        <a href="/">Lista notatek</a>
-                    </li>
-                    <li>
-                        <a href="/?action=create">Nowa notatka</a>
-                    </li>
-                </ul>
-            </div>
-
-            <div>
-                Nowa notatka
-                <?php echo htmlentities($action) ?>
-            </div>
-        </div>
-
-        <div class="footer">
-
-        </div>
-    </body>
-</html>
+<div>
+    Nowa notatka
+    <div>
+        Tutaj będzie formularz dla nowej notatki
+    </div>
+</div>
--- Project/templates/pages/list.php
+++ Project/templates/pages/list.php
@@ -1,31 +1,3 @@
-<html>
-    <head>
-
-    </head>
-    <body>
-        <div class="header">
-            <h1>Moje notatki</h1>
-        </div>
-
-        <div class="container">
-            <div class="menu">
-                <ul>
-                    <li>
-                        <a href="/">Lista notatek</a>
-                    </li>
-                    <li>
-                        <a href="/?action=create">Nowa notatka</a>
-                    </li>
-                </ul>
-            </div>
-
-            <div>
-                Lista notatek
-            </div>
-        </div>
-
-        <div class="footer">
-
-        </div>
-    </body>
-</html>
+<div>
+    Lista notatek
+</div>
