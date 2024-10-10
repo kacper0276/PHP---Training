ea4911cd28ec7c9d8b3aaa5c355a0112f21ef4b5
kacper0276 <kacper0276@op.pl>
2024-10-10 22:26:22 +0200

    Remove html from index.php

--- Project/index.php
+++ Project/index.php
@@ -12,43 +12,8 @@ require_once ('src/Utils/debug.php');
 
 $action = $_GET['action'] ?? null;
 
-?>
-
-<html>
-    <head>
-
-    </head>
-    <body>
-        <div class="header">
-            <h1>Moje notatki</h1>
-        </div>
-
-        <div class="container">
-            <div class="menu">
-                <ul>
-                    <li>
-                        <a href="/">Lista notatek</a>
-                    </li>
-                    <li>
-                        <a href="/?action=create">Nowa notatka</a>
-                    </li>
-                </ul>
-            </div>
-
-            <div>
-                <?php if($action === 'create') : ?>
-                    Nowa notatka
-                    <?php echo htmlentities($action) ?>
-                <?php else : ?>
-                    Lista notatek
-                    <?php echo htmlentities($action ?? '') ?>
-                <?php endif; ?>
-                ?>
-            </div>
-        </div>
-
-        <div class="footer">
-
-        </div>
-    </body>
-</html>
+if ($action === 'create') {
+    include_once('templates/pages/create.php');
+} else {
+    include_once('templates/pages/list.php');
+}
