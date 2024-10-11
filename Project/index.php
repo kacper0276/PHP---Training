71107fbd40cbbc4c952f06fd17f85c80b4c427b6
kacper0276 <kacper0276@op.pl>
2024-10-11 08:48:11 +0200

    Changes in index.php

--- Project/index.php
+++ Project/index.php
@@ -11,8 +11,18 @@ namespace App;
 require_once ('src/Utils/debug.php');
 require_once('src/View.php');
 
-$action = $_GET['action'] ?? null;
+const DEFAULT_ACTION = 'list';
+
+$action = $_GET['action'] ?? DEFAULT_ACTION;
 
 $view = new View();
-$view->render($action);
+
+$viewParams = [];
+if ($action === 'create') {
+    $viewParams['resultCreate'] = 'Udało się';
+} else {
+    $viewParams['resultList'] = 'Wyświetlamy notatki';
+}
+
+$view->render($action, $viewParams);
 
