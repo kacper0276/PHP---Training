04be8bce60026942978f971ec7e8175b798da471
kacper0276 <kacper0276@op.pl>
2024-10-13 22:24:22 +0200

    Small refactor

--- Project/src/View.php
+++ Project/src/View.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App;
 
 class View {
-    public function render(string $page, array $param): void {
+    public function render(string $page, array $params): void {
         require_once('templates/layout.php');
     }
 }
