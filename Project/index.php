97e29867adf065ade15022e447e28e959bab81e4
kacper0276 <kacper0276@op.pl>
2024-10-16 13:23:31 +0200

    Use new exceptions

--- Project/index.php
+++ Project/index.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace App;
 
+use App\Exception\AppException;
+use App\Exception\ConfigurationException;
+use Throwable;
+
 // include ('src/Utils/debug.php'); // On też pokazuje błąd ale nie przerywa działania skryptu (Warning)
 // include_once ('src/Utils/debug.php');
 
@@ -17,9 +21,18 @@ $request = [
   'post' => $_POST
 ];
 
-//$controller = new Controller($request);
-//$controller->run();
+try {
+  Controller::initConfiguration($configuration);
+  (new Controller($request))->run();
+} catch (ConfigurationException $e) {
+  echo '<h1>Wystąpił błąd w aplikacji</h1>';
+  echo 'Problem z applikacją, proszę spróbować za chwilę.';
+} catch (AppException $e) {
+  echo '<h1>Wystąpił błąd w aplikacji</h1>';
+  echo '<h3>' . $e->getMessage() . '</h3>';
+} catch (Throwable $e) {
+  echo '<h1>Wystąpił błąd w aplikacji</h1>';
+  dump($e);
+}
 
-Controller::initConfiguration($configuration);
-(new Controller($request))->run();
 
