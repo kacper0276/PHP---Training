0ac0419d50b33c9b487058ea520441c1e8edd0e3
kacper0276 <kacper0276@op.pl>
2024-10-20 14:44:11 +0200

    Changes imports in index php

--- Project/index.php
+++ Project/index.php
@@ -2,21 +2,22 @@
 
 declare(strict_types=1);
 
-namespace App;
+spl_autoload_register(function (string $classNamespace) {
+  $path = str_replace(['\\', 'App/'], ['/', ''], $classNamespace);
+  $path = "src/$path.php";
+  require_once($path);
+});
 
 require_once("src/Utils/debug.php");
-require_once("src/NoteController.php");
-require_once("src/Request.php");
-require_once("src/Exception/AppException.php");
+$configuration = require_once("config/config.php");
 
+use App\Controller\AbstractController;
+use App\Controller\NoteController;
 use App\Request;
 use App\Exception\AppException;
 use App\Exception\ConfigurationException;
-use Throwable;
 
-$configuration = require_once("config/config.php");
-
-$request = new Request($_GET, $_POST);
+$request = new Request($_GET, $_POST, $_SERVER);
 
 try {
   //$controller = new Controller($request);
@@ -30,7 +31,7 @@ try {
 } catch (AppException $e) {
   echo '<h1>Wystąpił błąd w aplikacji</h1>';
   echo '<h3>' . $e->getMessage() . '</h3>';
-} catch (Throwable $e) {
+} catch (\Throwable $e) {
   echo '<h1>Wystąpił błąd w aplikacji</h1>';
   dump($e);
 }
