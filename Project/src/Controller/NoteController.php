ab8b3f27268d7f27824c6e03650b78c368ec0e06
kacper0276 <kacper0276@op.pl>
2024-10-22 18:15:00 +0200

    Add pagination in controller

--- Project/src/Controller/NoteController.php
+++ Project/src/Controller/NoteController.php
@@ -7,6 +7,8 @@ namespace App\Controller;
 use App\Exception\NotFoundException;
 
 class NoteController extends AbstractController {
+  private const PAGE_SIZE = 10;
+
   public function createAction() {
     if ($this->request->hasPost()) {
       $noteData = [
@@ -14,42 +16,91 @@ class NoteController extends AbstractController {
         'description' => $this->request->postParam('description')
       ];
       $this->database->createNote($noteData);
-      header('Location: /?before=created');
-      exit;
+      $this->redirect('/', ['before' => 'created']);
     }
 
     $this->view->render('create');
   }
 
-  public function showAction() {
-    $noteId = (int) $this->request->getParam('id');
+  public function showAction(): void {
+    $this->view->render(
+      'show',
+      ['note' => $this->getNote()]
+    );
+  }
 
+  public function listAction(): void {
+    $pageNumber = (int) $this->request->getParam('page', 1);
+    $pageSize = (int) $this->request->getParam('pagesize', self::PAGE_SIZE);
+    $sortBy = $this->request->getParam('sortby', 'title');
+    $sortOrder = $this->request->getParam('sortorder', 'desc');
+
+    if (!in_array($pageSize, [1, 5, 10, 25])) {
+      $pageSize = self::PAGE_SIZE;
+    }
+
+    $note = $this->database->getNotes($pageNumber, $pageSize, $sortBy, $sortOrder);
+    $notes = $this->database->getCount();
+
+    $this->view->render(
+      'list',
+      [
+        'page' => [
+          'number' => $pageNumber,
+          'size' => $pageSize,
+          'pages' => (int) ceil($notes / $pageSize)
+        ],
+        'sort' => ['by' => $sortBy, 'order' => $sortOrder],
+        'notes' => $note,
+        'before' => $this->request->getParam('before'),
+        'error' => $this->request->getParam('error')
+      ]
+    );
+  }
+
+  public function editAction(): void {
+
+    if ($this->request->isPost()) {
+      $noteId = (int) $this->request->postParam('id');
+      $noteData = [
+        'title' => $this->request->postParam('title'),
+        'description' => $this->request->postParam('description')
+      ];
+      $this->database->editNote($noteId, $noteData);
+      $this->redirect('/', ['before' => 'edited']);
+    }
+
+    $this->view->render(
+      'edit',
+      ['note' => $this->getNote()]
+    );
+  }
+
+  public function deleteAction(): void {
+    if ($this->request->isPost()) {
+      $id = (int) $this->request->postParam('id');
+      $this->database->deleteNote($id);
+      $this->redirect('/', ['before' => 'deleted']);
+    }
+
+    $this->view->render(
+      'delete',
+      ['note' => $this->getNote()]
+    );
+  }
+
+  final private function getNote(): array {
+    $noteId = (int) $this->request->getParam('id');
     if (!$noteId) {
-      header('Location: /?error=missingNoteId');
-      exit;
+      $this->redirect('/', ['error' => 'missingNoteId']);
     }
 
     try {
       $note = $this->database->getNote($noteId);
     } catch (NotFoundException $e) {
-      header('Location: /?error=noteNotFound');
-      exit;
+      $this->redirect('/', ['error' => 'noteNotFound']);
     }
 
-    $this->view->render(
-      'show',
-      ['note' => $note]
-    );
-  }
-
-  public function listAction() {
-    $this->view->render(
-      'list',
-      [
-        'notes' => $this->database->getNotes(),
-        'before' => $this->request->getParam('before'),
-        'error' => $this->request->getParam('error')
-      ]
-    );
+    return $note;
   }
 }
