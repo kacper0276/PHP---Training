b023af5f478ce485d232236b7482e11d6bd07e1f
kacper0276 <kacper0276@op.pl>
2024-10-22 18:15:14 +0200

    Create delete page

--- Project/templates/pages/list.php
+++ Project/templates/pages/list.php
@@ -21,12 +21,51 @@
           case 'created':
             echo 'Notatka zostało utworzona';
             break;
+          case 'deleted':
+            echo 'Notatka została usunięta';
+            break;
           case 'edited':
             echo 'Notatka została zaktualizowana';
             break;
         }
       }
       ?>
+    </div>
+
+    <?php
+
+    $sort = $params['sort'] ?? [];
+    $by = $sort['by'] ?? 'title';
+    $order = $sort['order'] ?? 'desc';
+
+    $page = $params['page'] ?? [];
+    $size = $page['size'] ?? 10;
+    $currentPage = $page['number'] ?? 1;
+    $pages = $page['pages'] ?? 1;
+
+    ?>
+
+    <div>
+      <form class="settings-form" action="/" method="GET">
+        <div>
+          <div>Sortuj po:</div>
+          <label>Tytule: <input name="sortby" type="radio" value="title" <?php echo $by === 'title' ? 'checked' : '' ?> /></label>
+          <label>Dacie: <input name="sortby" type="radio" value="created" <?php echo $by === 'created' ? 'checked' : '' ?> /></label>
+        </div>
+        <div>
+          <div>Kierunek sortowania</div>
+          <label>Rosnąco: <input name="sortorder" type="radio" value="asc" <?php echo $order === 'asc' ? 'checked' : '' ?> /></label>
+          <label>Malejąco: <input name="sortorder" type="radio" value="desc" <?php echo $order === 'desc' ? 'checked' : '' ?> /></label>
+        </div>
+        <div>
+          <div>Rozmiar paczki</div>
+          <label>1 <input name="pagesize" type="radio" value="1" <?php echo $size === 1 ? 'checked' : '' ?> /></label>
+          <label>5 <input name="pagesize" type="radio" value="5" <?php echo $size === 5 ? 'checked' : '' ?> /></label>
+          <label>10 <input name="pagesize" type="radio" value="10" <?php echo $size === 10 ? 'checked' : '' ?> /></label>
+          <label>25 <input name="pagesize" type="radio" value="25" <?php echo $size === 25 ? 'checked' : '' ?> /></label>
+        </div>
+        <input type="submit" value="Wyślij" />
+      </form>
     </div>
 
     <div class="tbl-header">
@@ -53,11 +92,40 @@
                 <a href="/?action=show&id=<?php echo $note['id'] ?>">
                   <button>Szczegóły</button>
                 </a>
+                <a href="/?action=delete&id=<?php echo $note['id'] ?>">
+                  <button>Usuń</button>
+                </a>
               </td>
             </tr>
           <?php endforeach; ?>
         </tbody>
       </table>
     </div>
+
+    <?php
+    $paginationUrl = "&pagesize=$size?sortby=$by&sortorder=$order";
+    ?>
+    <ul class="pagination">
+      <?php if ($currentPage !== 1) : ?>
+        <li>
+          <a href="/?page=<?php echo $currentPage - 1 . $paginationUrl ?>">
+            <button> Prev </button>
+          </a>
+        </li>
+      <?php endif; ?>
+      <?php for ($i = 1; $i <= $pages; $i++) : ?> <li>
+          <a href="/?page=<?php echo $i . $paginationUrl ?>">
+            <button><?php echo $i ?></button>
+          </a>
+        </li>
+      <?php endfor; ?>
+      <?php if ($currentPage < $pages) : ?>
+        <li>
+          <a href="/?page=<?php echo $currentPage + 1 . $paginationUrl ?>">
+            <button> Next </button>
+          </a>
+        </li>
+      <?php endif; ?>
+    </ul>
   </section>
 </div>
